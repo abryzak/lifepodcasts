a863c69ca9e0705bd4446e248b515f66aff23dbf
Adam Bryzak <adam+github@bryzak.net>
2010-05-12 23:43:23 +1000

    Created die function for easier script quitting

--- post.sh
+++ post.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+function die {
+	echo "$@" >&2
+	exit 1
+}
+
 BINDIR=$(dirname "$0")
 DIR=$1
-[[ -d "$DIR" ]] || { echo "usage: $0 DIR" >&2; exit 1; }
+[[ -d "$DIR" ]] || die "usage: $0 DIR"
 
 DB="$DIR/.post.db3"
 if [[ ! -e "$DB" ]]; then
 	sqlite3 "$DB" "create table if not exists files (md5 text primary key, filename not null, file_url, announce_url)" ||
-		{ echo "$0: error creating db" >&2; exit 1; }
+		die "$0: error creating db"
 fi
 
-[[ -r "$DB" ]] || { echo "$0: db not readable" >&2; exit 1; }
+[[ -r "$DB" ]] || die "$0: db not readable"
 
 function escape_string {
 	echo -n "$@" | sed "s/'/''/g"
@@ -24,17 +29,17 @@ function is_postable_file {
 for FILE in "$DIR"/*; do
 	is_postable_file "$FILE" || continue
 	BASENAME=$(basename "$FILE")
-	MD5=$(openssl dgst -md5 <"$FILE") || { echo "$0: MD5 failed for file $FILE" >&2; exit 1; }
+	MD5=$(openssl dgst -md5 <"$FILE") || die "$0: MD5 failed for file $FILE"
 	# TODO handle renamed files
 	sqlite3 "$DB" "insert or ignore into files (md5, filename) values ('$(escape_string "$MD5")', '$(escape_string "$BASENAME")')" ||
-		{ echo "$0: unable to insert entry into db" >&2; exit 1; }
+		die "$0: unable to insert entry into db"
 done
 
 # find files that need posting
 unset KEYS
 unset FILES_TO_POST
 SELECT=$(sqlite3 "$DB" "select md5, filename from files where file_url is null") ||
-	{ echo "$0: couldn't find files to post" >&2; exit 1; }
+	die "$0: couldn't find files to post"
 while IFS=$'\n' read -r LINE; do
 	MD5=${LINE:0:32}
 	FILE="$DIR/${LINE:33}"
@@ -45,12 +50,12 @@ done <<< "$SELECT"
 
 # post the files and update db row
 FILE_URLS=$("$BINDIR"/post_file.py "${FILES_TO_POST[@]}") ||
-	{ echo "$0: failed to post files" >&2; exit 1; }
+	die "$0: failed to post files"
 IDX=0
 while IFS=$'\n' read -r FILE_URL; do
 	MD5=${KEYS[IDX]}
 	sqlite3 "$DB" "update files set file_url = '$(escape_string "$FILE_URL")' where md5 = '$(escape_string "$MD5")'" ||
-		{ echo "$0: failed to set file_url" >&2; exit 1; }
+		die "$0: failed to set file_url"
 	IDX=$((IDX + 1))
 done <<< "$FILE_URLS"
 
@@ -59,7 +64,7 @@ unset KEYS
 unset FILENAMES
 unset FILE_URLS
 SELECT=$(sqlite3 "$DB" "select md5, filename, file_url from files where announce_url is null") ||
-	{ echo "$0: couldn't find announcements to create" >&2; exit 1; }
+	die "$0: couldn't find announcements to create"
 while IFS=$'\n' read -r LINE; do
 	MD5=${LINE:0:32}
 	FILENAME=${LINE:33}
@@ -91,7 +96,7 @@ for (( IDX=0; IDX < ${#KEYS[@]}; ++IDX )); do
 	TITLE=$(title_from_filename "$FILENAME")
 	HTML=$(html_from_filename_and_url "$FILENAME" "$FILE_URL")
 	ANNOUNCE_URL=$("$BINDIR"/post_announcement.py "$TITLE" <<< "$HTML") ||
-		{ echo "$0: failed to post announcement for $FILENAME" >&2; exit 1; }
+		die "$0: failed to post announcement for $FILENAME"
 	sqlite3 "$DB" "update files set announce_url = '$(escape_string "$ANNOUNCE_URL")' where md5 = '$(escape_string "$MD5")'" ||
-		{ echo "$0: failed to set announce_url" >&2; exit 1; }
+		die "$0: failed to set announce_url"
 done
