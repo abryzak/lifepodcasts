d3dd420fe34cb0313d9639513b7625970a29d361
Adam Bryzak <adam+github@bryzak.net>
2010-05-30 07:52:01 +1000

    Added script for splitting a filename into parts
    This new script will allow for better announcement title's and contents
    to be used when creating an announcement on the Google site. If the
    script returns no output, the filename is in an unrecognised format.
    Otherwise, each line has a field.

---
+++ split_filename_into_parts.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import datetime
+import os.path
+import re
+import sys
+
+sys.argv=["","dir/2010_05_03_PM_Ps Sacha Pace_How to be A Good Worshipper.mp3"]
+
+if len(sys.argv) != 2:
+  sys.exit("usage: split_filename_into_parts.py filename")
+
+fn = sys.argv[1]
+fn = os.path.basename(fn)
+if fn.rfind(".") != -1:
+  fn = fn[:fn.rfind(".")]
+
+# grab the service date
+match = re.match(r"((?:\d{2,})?\d\d)[-_.](\d\d)[-_.](\d\d)[-_.]", fn)
+if match is None:
+  sys.exit()
+fn = fn[match.end():]
+year = match.group(1)
+if len(year) == 2:
+  year = str(datetime.date.today().year / 100) + year
+month = match.group(2)
+day = match.group(3)
+service_date = datetime.date(int(year), int(month), int(day))
+service_date_string = str(service_date.day) + service_date.strftime(" %b %Y")
+
+# grab the service type
+match = re.match(r"([^-_.]+)[-_.]", fn)
+if match is None:
+  sys.exit()
+fn = fn[match.end():]
+service_type = match.group(1).upper()
+service_type_string = None
+if service_type == "MSV":
+  service_type_string = "Massive"
+elif service_type == "PUP":
+  service_type_string = "PowerUp"
+elif re.match(r"(9(30)?)?AM$", service_type):
+  service_type_string = "Morning Service"
+elif re.match(r"((6|18)(00)?)?PM$", service_type):
+  service_type_string = "Generate"
+if service_type_string is None:
+  sys.exit()
+
+# grab the speaker
+match = re.match(r"([^_]+)[_]", fn)
+if match is None:
+  sys.exit()
+fn = fn[match.end():]
+service_speaker = match.group(1)
+
+# the rest is the title
+service_title = fn
+
+# now that we have all the parts - output nice versions on each line
+print service_date_string
+print service_type_string
+print service_speaker
+print service_title
